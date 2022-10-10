773f9015a3a87cc81353cc3e34163af1750a3a1a
abc <anhkhoa15062001@gmail.com>
2022-10-11 00:13:56 +0700

    doankhoaluan_mangxahoi

---
+++ base/views.py
@@ -0,0 +1,205 @@
+from email import message
+import email
+from multiprocessing import context
+from pydoc_data.topics import topics
+from unicodedata import name
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render,redirect 
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Message, Room, Topic,User
+from .forms import RoomForm, UserForm, MyUserCreationForm
+
+# Create your views here.
+
+# rooms = [
+#     {'id':1, 'name':'a'},
+#     {'id':2, 'name':'b'},
+#     {'id':3, 'name':'c'},
+# ]
+#Đăng nhập
+def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exits')
+
+        user = authenticate(request, email=email, password=password)  
+
+        if user is not None:
+            login(request, user) 
+            return redirect('home') 
+        else:
+            messages.error(request, 'Username Or Password Does Not Exit')    
+
+    context ={'page':page}
+    return render(request, 'base/login_register.html', context)
+#Đăng xuất
+def logoutUser(request):
+    logout(request)
+    return redirect('home')   
+#Đăng kí
+def registerPage(request):
+    form = MyUserCreationForm()
+
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()    
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Your registration information is not valid, please check again')    
+
+    return render(request, 'base/login_register.html', {'form':form})    
+
+#Trang chủ
+@login_required(login_url='/login')
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+
+    topics = Topic.objects.all()[0:6] 
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {'rooms':rooms, 'topics':topics,
+     'room_count':room_count, 'room_messages':room_messages}
+    return render(request, 'base/home.html', context)
+
+#Phòng đăng
+@login_required(login_url='/login')
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room,'room_messages':room_messages,
+     'participants':participants}
+    return render(request,'base/room.html',context)
+
+#Trang cá nhân
+@login_required(login_url='/login')
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms':rooms,
+     'room_messages':room_messages,'topics':topics}
+    return render(request, 'base/profile.html',context)    
+#Tạo Phòng
+@login_required(login_url='/login')
+def createRoom(request):
+    form = RoomForm()
+    topics = Topic.objects.all()
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+
+        return redirect('home')
+
+    context = {'form': form, 'topics':topics}
+    return render(request, 'base/room_form.html', context)
+#Cập nhật bài đăng
+@login_required(login_url='/login')
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    topics = Topic.objects.all()
+    if request.user != room.host:
+        return HttpResponse('Your are not allowed here!')
+
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+
+    context = {'form':form, 'topics':topics, 'room': room}
+    return render(request, 'base/room_form.html', context)   
+#Xóa Phòng
+@login_required(login_url='/login')
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('Your are not allowed here!')
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj':room})
+
+# Xóa tin nhắn
+@login_required(login_url='/login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Your are not allowed here!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj':message}) 
+
+#Cập nhật tài khoản cá nhân
+@login_required(login_url='/login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES , instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update-user.html', {'form': form})   
+
+#Trang Chủ đề
+@login_required(login_url='/login')
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html',{'topics':topics})
+#Trang  hoạt động
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
